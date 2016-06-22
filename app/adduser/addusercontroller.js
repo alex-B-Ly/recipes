502a9293e1ee78a95a678b8113a9c57e75408db7
alex-B-Ly <abl12582@gmail.com>
2016-06-22 02:27:51 -0400

    ng-click on form clears user object, info still need to be passed into db

--- app/adduser/addusercontroller.js
+++ app/adduser/addusercontroller.js
@@ -1,10 +1,12 @@
 var app = angular.module('recipesApp');
 
 app.controller('newUserController', ['$scope', function($scope){
-	$scope.user;
+	// $scope.user;
 
 	$scope.addUser = function(){
 		console.log($scope.user);
 		// TODO Pass user info into db
+		$scope.user = {}
+		$scope.user = undefined;
 	}
 }]);
